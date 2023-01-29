be580476c42e8966d9aba0e883c84ee901a997a9
Albina Zubiak <albnzubiak@gmail.com>
2023-01-29 18:18:39 +0100

    ready

---
+++ script.js
@@ -0,0 +1,65 @@
+//Задание 1
+let password = 'пароль';
+let answerUser = prompt('');
+if (answerUser === password) {
+    alert("Пароль введен верно");
+} else {
+    alert("Пароль введен неправильно");
+}
+
+//Задание 2
+
+let c = 9;
+if (c > 0 && c < 10) {
+    console.log(alert('Верно'));
+} else {
+    console.log(alert('Неверно'));
+}
+
+//Задание 3
+
+let d = 10;
+let e = 100;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+//Задание 4
+
+let a = 2;
+let b = 3;
+alert(a + b);
+
+//Задание 5
+
+let monthNumber = 12;
+switch (monthNumber) {
+    case '12':
+    case '1':
+    case '2':
+        alert('Зима');
+        break;
+
+    case '3':
+    case '4':
+    case '5':
+        alert('Весна');
+        break;
+
+    case '6':
+    case '7':
+    case '8':
+        alert('Лето');
+        break;
+
+    case '9':
+    case '10':
+    case '11':
+        alert('Осень');
+        break;
+    default:
+        alert('нет больше 12 месяцев');
+}
