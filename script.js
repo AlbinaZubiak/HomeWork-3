3b965e64aeac67b0e67bb082737adf5c20852c3b
Albina Zubiak <albnzubiak@gmail.com>
2023-01-29 20:38:34 +0100

    Corrected errors in task 1 and 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Задание 1
 let password = 'пароль';
-let answerUser = prompt('');
+let answerUser = prompt('Введите пароль');
 if (answerUser === password) {
     alert("Пароль введен верно");
 } else {
@@ -28,9 +28,9 @@ if (d > 100 || e > 100) {
 }
 
 //Задание 4
-
-let a = 2;
-let b = 3;
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
 alert(a + b);
 
 //Задание 5
